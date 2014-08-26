42843014fd5ad127ee193901aec67c51d7e04420
buonzz <buonzz@gmail.com>
2014-08-26 08:07:25 +0800

    ignored some files

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-php bin/satis build config.json web/ --no-interaction
+rm -rf /var/share/darwin/buonzz.com/packages/satis/web/dist 
+php /var/share/darwin/buonzz.com/packages/satis/bin/satis build /var/share/darwin/buonzz.com/packages/satis/config.json /var/share/darwin/buonzz.com/packages/satis/web --no-interaction
