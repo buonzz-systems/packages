21aabf99ccee4cd58773789ec75c57aeaaa4c94c
buonzz <buonzz@gmail.com>
2014-08-30 18:50:40 +0800

    modified new layout and added l4

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 rm -rf /var/share/darwin/buonzz.com/packages/satis/web/dist 
-php /var/share/darwin/buonzz.com/packages/satis/bin/satis build /var/share/darwin/buonzz.com/packages/satis/config.json /var/share/darwin/buonzz.com/packages/satis/web --no-interaction
+php /var/share/darwin/buonzz.com/packages/satis/bin/satis build /var/share/darwin/buonzz.com/packages/satis/config.json /var/share/darwin/buonzz.com/packages/satis/web --no-interaction --skip-errors
