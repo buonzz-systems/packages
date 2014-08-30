9d1035827b6ca99cd3bc541c6f510e0625bf1fae
buonzz <buonzz@gmail.com>
2014-08-30 23:35:44 +0800

    added commands for git

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 #rm -rf /var/share/darwin/buonzz.com/packages/satis/web/dist 
 php /var/share/darwin/buonzz.com/packages/satis/bin/satis build /var/share/darwin/buonzz.com/packages/satis/config.json /var/share/darwin/buonzz.com/packages/satis/web --no-interaction --skip-errors
 cd /var/share/darwin/buonzz.com/packages/satis/web
+git add --all
+git commit -m "satis build"
 phploy
