7f680e3fd49de2e7221a1b563fae393333c1de06
buonzz <buonzz@gmail.com>
2014-08-30 23:22:41 +0800

    added phploy

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
-rm -rf /var/share/darwin/buonzz.com/packages/satis/web/dist 
+#rm -rf /var/share/darwin/buonzz.com/packages/satis/web/dist 
 php /var/share/darwin/buonzz.com/packages/satis/bin/satis build /var/share/darwin/buonzz.com/packages/satis/config.json /var/share/darwin/buonzz.com/packages/satis/web --no-interaction --skip-errors
+cd /var/share/darwin/buonzz.com/packages/satis/web
+phploy
